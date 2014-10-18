7211d77b2e968f25c3482cae37100f16f3a8a737
Jonathan Sterling <jon@jonmsterling.com>
2014-10-18 11:33:48 -0700

    only expose what I need from string_utils

--- src/pretty/string_utils.rs
+++ src/pretty/string_utils.rs
@@ -1,4 +1,4 @@
-pub fn replicate<A:Clone>(x:A, l:uint) -> Vec<A> {
+fn replicate<A:Clone>(x:A, l:uint) -> Vec<A> {
     let i = 0u;
     let mut res = Vec::new();
     for _ in range(i,l) {
@@ -7,7 +7,7 @@ pub fn replicate<A:Clone>(x:A, l:uint) -> Vec<A> {
     res
 }
 
-pub fn pad_right(c:char, l:uint, str:String) -> String {
+fn pad_right(c:char, l:uint, str:String) -> String {
     let str_len = str.len();
     if l > str_len {
         let padding = replicate(String::from_chars([c]), l - str_len).concat();
@@ -19,7 +19,7 @@ pub fn pad_right(c:char, l:uint, str:String) -> String {
     }
 }
 
-pub fn pad_left(c:char, l:uint, str:String) -> String {
+fn pad_left(c:char, l:uint, str:String) -> String {
     let str_len = str.len();
     if l > str_len {
       let mut res = replicate(String::from_chars([c]), l - str_len).concat();
