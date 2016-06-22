90a8b81125606beeb0545f14fe650235d13bca03
Darin Morrison <darinmorrison+git@gmail.com>
2016-06-21 18:05:49 -0600

    Update docs

--- doc/implementors/core/cmp/trait.Ord.js
+++ doc/implementors/core/cmp/trait.Ord.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['pretty'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='struct' href='pretty/struct.Doc.html' title='pretty::Doc'>Doc</a>&lt;'a&gt;",];
+implementors['pretty'] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='struct' href='pretty/struct.Doc.html' title='pretty::Doc'>Doc</a>&lt;'a&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
